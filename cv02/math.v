9874a09df3b550fcf1dfb3ee1b91aecf909a3644
Petr Laštovička <lastope2@fit.cvut.cz>
2022-09-30 22:19:11 +0200

    lint(cv02): Configured linting

--- cv02/math.v
+++ cv02/math.v
@@ -1,19 +1,18 @@
-module adder(input [31:0] i_a, i_b, input i_c0, output [31:0] o_sum, output o_c32);
 
+module m_adder(input [31:0] i_a, i_b, input i_c0, output [31:0] o_sum, output o_c32);
   assign {o_c32, o_sum} = i_a + i_b + i_c0;
-
 endmodule
 
-module multiply4(input [31:0] i_d0, output [31:0] o_out);
+module m_multiply4(input [31:0] i_d0, output [31:0] o_out);
   wire [1:0] trash;
   assign {trash, o_out} = 4 * i_d0;
 endmodule
 
-module equal(input [31:0] i_d0, i_d1, output o_out);
+module m_equal(input [31:0] i_d0, i_d1, output o_out);
   assign o_out = i_d0 == i_d1;
 endmodule
 
-module expand(input [15:0] i_d0, output [31:0] o_out);
+module m_expand(input [15:0] i_d0, output [31:0] o_out);
   assign o_out = {{16{i_d0[15]}}, i_d0};
 endmodule
 
