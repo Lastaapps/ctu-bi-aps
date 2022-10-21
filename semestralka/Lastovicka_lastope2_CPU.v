96280046b5f6ae931ab0d30290b3fecfa8cb912d
Petr Laštovička <lastope2@fit.cvut.cz>
2022-10-21 14:56:57 +0200

    cpu: Decoder and ALU added

--- semestralka/Lastovicka_lastope2_CPU.v
+++ semestralka/Lastovicka_lastope2_CPU.v
@@ -38,39 +38,75 @@ endmodule
 
 
 /** Operation codes decoder
-  * @param i_opt - operation code
-  * @return o_aluCtl - controlls ALU
-  * @return o_aluSrc - chooses between register(0) and address(1) ALU intput
-  * @return o_regWrite - enabled write into registers
-  * @return o_mem - memori write enable
-  * @return o_branch - branching is happening
   */
-//module m_controller(input [2:0] i_opt,
-//                    output o_immControl,
-//                    output o_aluSrc,
-//                    output [1:0] o_aluCtl,
-//                    output o_mem_write,
-//                    output o_mem_to_reg_write,
-//                    output o_reg_write,
-//                    output o_br_jalr,
-//                    output o_br_jal,
-//                    output o_br_beq
-//                  );
-//  reg [5:0] out;
-//  assign {o_aluCtl, o_aluSrc, o_regWr, o_memWr, o_br} = out;
-//
-//  always @(*)
-//    case (i_opt)
-//      //opt          aCtl aSrc regWr memWr branch
-//      'b000: out <= 'b00_____0_____1_____0______0; // add
-//      'b001: out <= 'b00_____1_____1_____0______0; // addi
-//      'b010: out <= 'b01_____0_____1_____0______0; // sub
-//      'b011: out <= 'b00_____1_____1_____0______0; // lw
-//      'b100: out <= 'b00_____1_____0_____1______0; // sw
-//      'b101: out <= 'b01_____0_____0_____0______1; // beq
-//      default: out <= 0;
-//    endcase
-//endmodule
+module m_controller(input [31:0] i_inst,
+                    output [2:0] o_imm_ctl,
+                    output o_alu_src, // 0 - register, 1 - imm
+                    output [3:0] o_alu_ctl,
+                    output o_mem_write,
+                    output o_mem_to_reg,
+                    output o_reg_write,
+                    output o_imm_to_reg,
+                    output o_br_jalr,
+                    output o_br_jal,
+                    output o_br_beq,
+                    output o_br_bne,
+                    output o_br_blt,
+                    output o_aui);
+
+  wire s_add  = 'b000_0_0000_0010_00000_0; // add
+  wire s_addi = 'b001_1_0000_0010_00000_0; // add immediate
+  wire s_and  = 'b000_0_0010_0010_00000_0; // and
+  wire s_sub  = 'b000_0_0001_0010_00000_0; // substitute
+  wire s_slt  = 'b000_0_0011_0010_00000_0; // compare, a < b -> 1
+  wire s_div  = 'b000_0_0100_0010_00000_0; // divide
+  wire s_rem  = 'b000_0_0101_0010_00000_0; // modulo
+
+  wire s_beq  = 'b011_1_0001_0000_00100_0; // branch equal
+  wire s_bne  = 'b011_1_0001_0000_00010_0; // branch not equal
+  wire s_blt  = 'b011_1_0000_0000_00001_0; // jump if a < b
+
+  wire s_lw   = 'b001_1_0000_0100_00000_0; // load word
+  wire s_sw   = 'b010_1_0000_1000_00000_0; // save word
+  wire s_lui  = 'b100_1_0000_0001_00000_0; // load immediate
+
+  wire s_jal  = 'b101_0_0000_0000_01000_0; // jump relative to PC
+  wire s_jalr = 'b001_1_0000_0000_10000_0; // jump relative to rd
+  wire s_aui  = 'b100_0_0000_0000_00000_1; // rd <- r1 + PC
+
+  wire s_sll  = 'b000_0_0110_0010_00000_0; // logical left shift
+  wire s_srl  = 'b000_0_0111_0010_00000_0; // logical left shift
+  wire s_sra  = 'b000_0_1000_0010_00000_0; // arithmetic left shift
+
+  assign {
+    o_imm_ctl, o_alu_src, o_alu_ctl,
+    o_mem_write, o_mem_to_reg, o_reg_write, o_imm_to_reg,
+    o_br_jalr, o_br_jal, o_br_beq, o_br_bne, o_br_blt, o_aui
+    } = 
+      i_inst == 'b0000000_?????_?????_000_?????_0110011 ? s_add  : // add
+      i_inst == 'b???????_?????_?????_000_?????_0010011 ? s_addi : // addi
+      i_inst == 'b0000000_?????_?????_111_?????_0110011 ? s_and  : // and
+      i_inst == 'b0100000_?????_?????_000_?????_0110011 ? s_sub  : // sub
+      i_inst == 'b0000000_?????_?????_010_?????_0110011 ? s_slt  : // slt
+      i_inst == 'b0000001_?????_?????_100_?????_0110011 ? s_div  : // div
+      i_inst == 'b0000001_?????_?????_110_?????_0110011 ? s_rem  : // rem
+      i_inst == 'b0000000_?????_?????_001_?????_0110011 ? s_sll  : // sll
+      i_inst == 'b0000000_?????_?????_101_?????_0110011 ? s_srl  : // srl
+      i_inst == 'b0100000_?????_?????_101_?????_0110011 ? s_sra  : // sra
+
+      i_inst == 'b???????_?????_?????_000_?????_1100011 ? s_beq  : // beq
+      i_inst == 'b???????_?????_?????_001_?????_1100011 ? s_bne  : // bne
+      i_inst == 'b???????_?????_?????_100_?????_1100011 ? s_blt  : // blt
+
+      i_inst == 'b???????_?????_?????_010_?????_0000011 ? s_lw   : // lw
+      i_inst == 'b???????_?????_?????_010_?????_0100011 ? s_sw   : // sw
+      i_inst == 'b???????_?????_?????_???_?????_0110111 ? s_lui  : // lui
+
+      i_inst == 'b???????_?????_?????_???_?????_1101111 ? s_jal  : // jal
+      i_inst == 'b???????_?????_?????_000_?????_1100111 ? s_jalr : // jalr
+      i_inst == 'b???????_?????_?????_???_?????_0010111 ? s_aui  : // auipc
+      0;
+endmodule
 
 /* Decodes data stored in an instruction
 * R - 000
@@ -127,27 +163,43 @@ endmodule
 /** ALU
   * @param i_d0, i_d1 - registers to sum
   * @param k_ctl - mode control
-  *   00 - and
-  *   01 - sub
-  *   10 - xor
+  *   0000 - add
+  *   0001 - sub
+  *   0010 - and
+  *   0011 - slt
+  *   0100 - div
+  *   0101 - rem
+  *   0110 - sll
+  *   0111 - srl
+  *   1000 - sra
   * @return o_data - operation result
   * @return o_zero - 1 if the operation result is 0
   */
-module m_alu(input [31:0] i_d0, i_d1, input [1:0] i_ctl, output reg [31:0] o_data, output o_zero);
-  wire [31:0] add, sub, x_or;
-  assign add = i_d0 + i_d1;
-  assign sub = i_d0 - i_d1;
-  assign x_or = i_d0 ^ i_d1;
-  assign o_zero = o_data == 0;
+module m_alu(input [31:0] i_d0, i_d1, input [1:0] i_ctl, output [31:0] o_data, output o_zero);
+  wire [31:0] w_add = i_d0 + i_d1;
+  wire [31:0] w_sub = i_d0 - i_d1;
+  wire [31:0] w_and = i_d0 ^ i_d1;
+  wire [31:0] w_slt = i_d0 < i_d1 ? 1 : 0;
+  wire [31:0] w_div = i_d0 / i_d1;
+  wire [31:0] w_rem = i_d0 % i_d1;
+  wire [31:0] w_sll = i_d0 << i_d1;
+  wire [31:0] w_srl = i_d0 >> i_d1;
+  wire [31:0] w_sra = i_d0 >>> i_d1;
 
-  always @(*) begin
-    case (i_ctl)
-      0: o_data <= add;
-      1: o_data <= sub;
-      2: o_data <= x_or;
-      default: o_data <= 0;
-    endcase
-  end
+  wire [31:0] result =
+    'b0000 ? w_add :
+    'b0001 ? w_sub :
+    'b0010 ? w_and :
+    'b0011 ? w_slt :
+    'b0100 ? w_div :
+    'b0101 ? w_rem :
+    'b0110 ? w_sll :
+    'b0111 ? w_srl :
+    'b1000 ? w_sra :
+    0;
+
+  assign o_zero = result == 0;
+  assign o_data = result;
 endmodule
 
 
